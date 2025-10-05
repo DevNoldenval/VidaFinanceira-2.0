cff0b3091e84881246c9962640f4c99856527074
Noldenval Santana <noldenval.dev@gmail.com>
2025-10-05 15:09:33 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,29 +1,23 @@
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g",
-  authDomain: "controlemeubolso.firebaseapp.com",
-  databaseURL: "https://controlemeubolso-default-rtdb.firebaseio.com",
-  projectId: "controlemeubolso",
-  storageBucket: "controlemeubolso.firebasestorage.app",
-  messagingSenderId: "369393280169",
-  appId: "1:369393280169:web:0b0d768f074e0ff4c775b8",
-  measurementId: "G-0EYHJF137F"
+    apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g",
+    authDomain: "controlemeubolso.firebaseapp.com",
+    databaseURL: "https://controlemeubolso-default-rtdb.firebaseio.com",
+    projectId: "controlemeubolso",
+    storageBucket: "controlemeubolso.firebasestorage.app",
+    messagingSenderId: "369393280169",
+    appId: "1:369393280169:web:0b0d768f074e0ff4c775b8",
+    measurementId: "G-0EYHJF137F"
 };
 
-// Importar Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-// Exportar para uso em outros módulos
-export { firebaseConfig, db, app };
+// Habilitar offline persistence
+firebase.firestore().enablePersistence()
+    .catch((err) => {
+        console.error("Erro ao habilitar persistência offline:", err);
+    });
 
-// Tornar disponível globalmente para compatibilidade
-window.firebaseConfig = firebaseConfig;
-window.db = db;
-window.app = app;
-
-console.log("✅ Firebase configurado com sucesso!");
+console.log("Firebase configurado com sucesso!");
