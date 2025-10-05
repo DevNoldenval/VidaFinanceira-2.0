e48655fee2d3314e6997390fdaccb8864a022de2
Noldenval Santana <noldenval.dev@gmail.com>
2025-10-05 13:07:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 // Importações do Firebase
-// Importações do Firebase
-import { db } from './firebase-config.js';
-import { collection, getDocs, query, where, orderBy, limit, doc, getDoc, addDoc, updateDoc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getFirestore, collection, getDocs, query, where, orderBy, limit, doc, getDoc, addDoc, updateDoc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g",
+  authDomain: "controlemeubolso.firebaseapp.com",
+  databaseURL: "https://controlemeubolso-default-rtdb.firebaseio.com",
+  projectId: "controlemeubolso",
+  storageBucket: "controlemeubolso.firebasestorage.app",
+  messagingSenderId: "369393280169",
+  appId: "1:369393280169:web:0b0d768f074e0ff4c775b8",
+  measurementId: "G-0EYHJF137F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // Esperar o DOM carregar
 document.addEventListener('DOMContentLoaded', async () => {
@@ -1654,4 +1669,3 @@ document.addEventListener('DOMContentLoaded', async () => {
         hideLoading();
     }
 });
-
